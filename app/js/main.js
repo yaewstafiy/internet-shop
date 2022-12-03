bcc0dc126aa2ad41d274b535247878f6d8a80a0f
yaewstafiy <stasik7771@gmail.com>
2022-12-03 21:14:29 +0300

    added more in product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,16 @@
 $(function () {
 
 
+  $('.product-tabs__top-item').on('click', function (e){
+    e.preventDefault();
+    $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
+    $(this).addClass('product-tabs__top-item--active');
+
+    $('.product-tabs__content-item').removeClass('product-tabs__content-item--active');
+    $($(this).attr('href')).addClass('product-tabs__content-item--active');
+
+  });
+
   $('.product-slide__thumb').slick({
     asNavFor: '.product-slide__big',
     focusOnSelect: true,
@@ -54,6 +64,13 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+  $('.star-review').rateYo({
+    starWidth: "20px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    fullStar: true,
+  })
 
   $('.star').rateYo({
     starWidth: "17px",
